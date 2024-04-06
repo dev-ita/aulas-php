aae96d1e1dc6b596bfe6bd413fc86d7ef02fcaf1
dev-ita <italo.ods@hotmail.com>
2024-04-06 00:33:04 -0300

    requisição de arquivos

---
+++ introducao/requisicao-de-arquivos/index.php
@@ -0,0 +1,13 @@
+<?php
+
+use math\util\Math;
+
+use function math\util\quadrado;
+
+require "quadrado.php";
+
+echo Math::quadrado(10);
+echo "<br>";
+echo quadrado(10);
+
+?>
---
+++ introducao/requisicao-de-arquivos/quadrado.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace math\util;
+
+class Math
+{
+    public static function quadrado($numero)
+    {
+        return $numero * $numero;
+    }
+}
+
+function quadrado($numero)
+{
+    return $numero * $numero;
+}
+
+?>
